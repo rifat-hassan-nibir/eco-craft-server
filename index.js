332bce3d83366eabddacf525ab55e50c344bcfb5
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2024-04-29 09:46:04 +0600

    vercel.json file added and backend deployed to vercel

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const allCraftsCollection = client.db("artAndCraftDB").collection("juteAndWoodenCrafts");
 
@@ -89,7 +89,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
